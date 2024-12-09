cc3bb9856aa15595bcda1611dc22e201837ba260
omarhassaneg <omar.hassan@glamic.com>
2024-12-09 17:56:22 +0300

    update use cases lead qualification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  vite: {
+    ssr: {
+      noExternal: ['@astrojs/client']
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'astro-client': ['@astrojs/client']
+          }
+        }
+      }
+    }
+  }
 });
